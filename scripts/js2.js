0997716a746ccf5486e19c894692b149f24bab2e
IvanBazan <i-baz.1@ya.ru>
2020-11-16 20:13:13 +0300

    Number guessing game added

--- scripts/js2.js
+++ scripts/js2.js
@@ -7,7 +7,21 @@ info.textContent = 'Below is a dynamic list. Click anywhere outside the list to 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-html.onclick = function() {
+document.addEventListener("DOMContentLoaded", function() {
+    function createParagraph() {
+        let para = document.createElement('p');
+        para.textContent = 'You clicked the button!';
+        document.body.appendChild(para);
+    }
+
+    const buttons = document.querySelectorAll('button');
+
+    for(let i = 0; i < buttons.length ; i++) {
+        buttons[i].addEventListener('click', createParagraph);
+    }
+});
+
+list.onclick = function() {
     const listItem = document.createElement('li');
     const listContent = prompt('What content do you want the list item to have?');
     listItem.textContent = listContent;
@@ -18,4 +32,20 @@ html.onclick = function() {
         const listContent = prompt('Enter new content for your list item');
         this.textContent = listContent;
     }
+}
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector('.guesses');
+var lastResult = document.querySelector('.lastResult');
+var lowOrHi = document.querySelector('.lowOrHi');
+
+var guessSubmit = document.querySelector('.guessSubmit');
+var guessField = document.querySelector('.guessField');
+
+var guessCount = 1;
+var resetButton;
+
+function checkGuess() {
+    alert('I am a placeholder');
 }
