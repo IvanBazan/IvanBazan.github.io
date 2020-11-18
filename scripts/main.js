0ce2a32ad44f55c9a1273e6395eab93155ab53bf
IvanBazan <i-baz.1@ya.ru>
2020-11-18 18:30:33 +0300

    svg linkbar

--- scripts/main.js
+++ scripts/main.js
@@ -30,8 +30,45 @@ myButton.onclick = function() {
     setUserName();
 }
 
-var button3 = document.getElementById('b3')
+let button3 = document.getElementById('b3')
 
 button3.onclick = function () {
     window.location.href = '3.html';
+}
+
+let linkBar = document.querySelector('svg')
+
+let textLink = linkBar.childNodes[9]
+textLink.onmouseover = function () {
+    const randomColor = randomValueFromArray(['purple', 'red', 'blue','white']);
+    textLink.setAttribute('fill', randomColor);
+}
+textLink.onmouseout = function () {
+    textLink.setAttribute('fill', 'white');
+}
+textLink.onclick = function () {
+    window.location.href = '1.html';
+}
+
+let bigCircle = linkBar.childNodes[3]
+let circle = linkBar.childNodes[5]
+
+circle.onmouseover = function () {
+    circle.setAttribute('r', '45%');
+    const oldR = parseInt(bigCircle.getAttribute('r'));
+    bigCircle.setAttribute('r', oldR + 1 + "%" );
+
+}
+circle.onmouseout = function () {
+    circle.setAttribute('r', '50%');
+    const random = Math.floor(100 - Math.random() * 15) + '%';
+    circle.setAttribute('cy', random);
+}
+circle.onclick = function () {
+    window.location.href = '2.html';
+}
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
 }
